a9838b1a16e574efc4b51f4c7254804e215ffadf
daniellemujica <daniellemujica@yahoo.com>
2014-02-11 14:54:09 -0800

    added aminate.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 $(document).foundation();
 
-$('.box').transition({ 
-	x: '40px',
-	y: '100px', 
-perspective: '100px',
-  rotate3d: '1,1,0,180deg'
-}, 9000)
-		.transition({
-	x: '40px',
-});
+$('.box').addClass('animated fadeInDownBig')
 
 console.log("ok usa!");
